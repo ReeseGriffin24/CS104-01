f93f477be7b2848d089c7affb38f02d3f5e07753
ReeseGriffin24 <55414254+ReeseGriffin24@users.noreply.github.com>
2020-02-10 09:32:49 -0500

    Add files via upload

---
+++ Reese Griffin Pythonlf. CS 104.py
@@ -0,0 +1,36 @@
+Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Reese Griffin")
+Reese Griffin
+>>> 
+>>> print("Reese"+"Griffin")
+ReeseGriffin
+>>> print (1+2)
+3
+>>> print("Reese"+""+"Griffin")
+ReeseGriffin
+>>> print(1+"Reese")
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    print(1+"Reese")
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+>>> 
+>>> a=1
+>>> b=2
+>>> print (a+b)
+3
+>>> a=3
+>>> print(a+b)
+5
+>>> 
+>>> c=1
+>>> d=5
+>>> e= c+d
+>>> print ("e")
+e
+>>> print e
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(e)?
+>>> print(e)
+6
+>>> 
